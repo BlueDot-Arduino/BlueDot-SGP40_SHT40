deb4dbe2f3ff9a831139e9b21dc2f72a05605d1e
BlueDot <BlueDot-Arduino@users.noreply.github.com>
2023-04-23 15:29:48 +0200

    Update SGP40_SHT40_Test.ino

--- examples/SGP40_SHT40_Test/SGP40_SHT40_Test.ino
+++ examples/SGP40_SHT40_Test/SGP40_SHT40_Test.ino
@@ -1,45 +1,139 @@
-#include "Adafruit_SGP40.h"
-#include "Adafruit_SHT31.h"
 
+/***************************************************************************
+  Example for the SGP40+SHT40 board
+  written by Thiago Barros for BlueDot UG (haftungsbeschränkt)
+  BSD License
+  
+  Version 1.0.0 (2023-04-22)
+
+  This sketch was written for the sensors SGP40 and SHT40 from Sensirion and is based on the Adafruit libraries for these sensors.
+  The SGP40 is a device for measuring volatile organic compounds (VOC) and for evaluating indoor air quality.
+  The SHT40 is a digital temperature and relative humidity sensor.
+  The VOC Algorithm from Sensirion integrates the output values from teh SHT40 to improve the calculation of the VOC index.
+  For more technical information on the SGP40 and on the SHT40, please go to ------> http://www.bluedot.space
+
+ ***************************************************************************/
+
+#include "Adafruit_SGP40.h"
+#include "Adafruit_SHT4x.h"
 Adafruit_SGP40 sgp;
-Adafruit_SHT31 sht31;
+Adafruit_SHT4x sht4;
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) { delay(10); } // Wait for serial console to open!
+  Serial.println();
+  Serial.println();
+  Serial.println(F("##################################"));
+  Serial.println(F("SGP40 test with SHT40 compensation"));
 
-  Serial.println("SGP40 test with SHT31 compensation");
 
-  if (! sgp.begin()){
-    Serial.println("SGP40 sensor not found :(");
-    while (1);
-  }
+  //*********************************************************************
+  //*************ADVANCED SETUP - SAFE TO IGNORE!************************        
 
-  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
-    Serial.println("Couldn't find SHT31");
-    while (1);
-  }
+   //Here we can configure the SHT40 Temperature and Humidity Sensor
+   //First we set the measurement precision
+   //There are three precision levels: High, Medium and Low
+   //The precision levels direclty affect the measurement duration, noise level and energy consumption
+   //On doubt, just leave it on default (High precision)
+   sht4.setPrecision(SHT4X_HIGH_PRECISION);
 
-  Serial.print("Found SHT3x + SGP40 serial #");
-  Serial.print(sgp.serialnumber[0], HEX);
-  Serial.print(sgp.serialnumber[1], HEX);
-  Serial.println(sgp.serialnumber[2], HEX);
-}
-
-int counter = 0;
-void loop() {
-  uint16_t sraw;
-  int32_t voc_index;
   
-  float t = sht31.readTemperature();
-  Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
-  float h = sht31.readHumidity();
-  Serial.print("Hum. % = "); Serial.println(h);
+  switch (sht4.getPrecision()) {
+     case SHT4X_HIGH_PRECISION: 
+       Serial.println(F("SHT40 set to High precision"));
+       break;
+     case SHT4X_MED_PRECISION: 
+       Serial.println(F("SHT40 set to Medium precision"));
+       break;
+     case SHT4X_LOW_PRECISION: 
+       Serial.println(F("SHT40 set to Low precision"));
+       break;
+  }   
+  //*********************************************************************
+  //*************ADVANCED SETUP - SAFE TO IGNORE!************************       
+  
+  // The SHT40 has a built-in heater, which can be used for self-decontamination.
+  // The heater can be used for periodic creep compensation in prolongued high humidity exposure.
+  // For normal operation, leave the heater turned off.
 
+  sht4.setHeater(SHT4X_NO_HEATER);
+
+  
+  switch (sht4.getHeater()) {
+     case SHT4X_NO_HEATER: 
+       Serial.println(F("SHT40 Heater turned OFF"));
+       break;
+     case SHT4X_HIGH_HEATER_1S: 
+       Serial.println(F("SHT40 Heater: High heat for 1 second"));
+       break;
+     case SHT4X_HIGH_HEATER_100MS: 
+       Serial.println(F("SHT40 Heater: High heat for 0.1 second"));
+       break;
+     case SHT4X_MED_HEATER_1S: 
+       Serial.println(F("SHT40 Heater: Medium heat for 1 second"));
+       break;
+     case SHT4X_MED_HEATER_100MS: 
+       Serial.println(F("SHT40 Heater: Medium heat for 0.1 second"));
+       break;
+     case SHT4X_LOW_HEATER_1S: 
+       Serial.println(F("SHT40 Heater: Low heat for 1 second"));
+       break;
+     case SHT4X_LOW_HEATER_100MS: 
+       Serial.println(F("SHT40 Heater: Low heat for 0.1 second"));
+       break;
+  }
+
+  
+  //*********************************************************************
+  //*************ADVANCED SETUP IS OVER - LET'S CHECK THE CHIP ID!*******
+
+  
+  if (! sgp.begin())
+  {
+    Serial.println(F("SGP40 sensor not found!"));
+    while (1);  }
+  else
+  {
+    Serial.print(F("SGP40 detected!\t"));
+    Serial.print(F("Serial number:\t"));
+    Serial.print(sgp.serialnumber[0], HEX);
+    Serial.print(sgp.serialnumber[1], HEX);
+    Serial.println(sgp.serialnumber[2], HEX);    
+  }
+  
+  
+  if (! sht4.begin()) {
+    Serial.println(F("SHT40 sensor not found!"));
+    while (1) ;
+  }
+   else
+  {
+    Serial.print(F("SHT40 detected!\t"));
+    Serial.print(F("Serial number:\t"));
+    Serial.println(sht4.readSerial(), HEX);    
+  } 
+  Serial.println(F("----------------------------------"));
+ 
+}
+  //*********************************************************************
+  //*************NOW LET'S START MEASURING*******************************
+  
+void loop() {
+  
+  sensors_event_t humidity, temp;
+  sht4.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  
+  float t = temp.temperature;
+  Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
+  float h = humidity.relative_humidity;
+  Serial.print("Hum. % = "); Serial.print(h); Serial.print("\t\t");
+
+  uint16_t sraw;
   sraw = sgp.measureRaw(t, h);
   Serial.print("Raw measurement: ");
-  Serial.println(sraw);
+  Serial.print(sraw); Serial.print("\t\t");
 
+  int32_t voc_index;
   voc_index = sgp.measureVocIndex(t, h);
   Serial.print("Voc Index: ");
   Serial.println(voc_index);
